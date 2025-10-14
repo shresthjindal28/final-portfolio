9e68341efa245f4e3a1a06b301ecd908eadce908
shresthjindal28 <shresthjindal28@gmail.com>
2025-10-14 16:46:26 +0530

    Fix Vercel build by updating pnpm-lock.yaml; exclude static assets from sitemap and regenerate

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,4 +3,20 @@ module.exports = {
   siteUrl: 'https://www.shresthjindal.com',  // 👈 use the www version
   generateRobotsTxt: true,
   sitemapSize: 5000,
+  // Exclude non-page assets and private routes from sitemap
+  exclude: [
+    '/opengraph-image.png',
+    '/icon0.svg',
+    '/icon1.png',
+    '/apple-icon.png',
+    '/logo.png',
+    '/twitter-card.png',
+    '/web-app-manifest-192x192.png',
+    '/web-app-manifest-512x512.png',
+    '/manifest.json',
+    '/robots.txt',
+    '/sitemap.xml',
+    '/sitemap-0.xml',
+    '/privatePage',
+  ],
 }
